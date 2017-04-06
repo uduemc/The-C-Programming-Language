7971aabf9fffc34c0b07102ad71b6ba0fba2a6ea
uduemc <uduemc@gmail.com>
2017-04-06 11:31:00 +0800

    unknow

---
+++ 2/2-7/2-7-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+unsigned invert(unsigned x, int p, int n)
+{
+	return x ^ (~(~0U << n) << p);
+}
+
+int main()
+{
+	unsigned x;
+	int p, n;
+	for(x = 0; x< 700; x+=49)
+		for(n = 1; n < 8; n++)
+			for (p = 1; p < 8; p++)
+				printf("%u, %d, %d: %u\n",x, n, p, invert(x, n, p));
+	return 0;
+}
